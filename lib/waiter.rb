fa3d8be6de24b3d3a6d37f6a8dd7254ebfa888d6
ariel murphy <arielladders@gmail.com>
2020-08-04 02:24:33 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -11,5 +11,18 @@ class Waiter
   def self.all 
     @@all 
   end 
+  
+  def meals
+    Meal.all.select do |meal|
+      meal.waiter == self 
+    end 
+  end 
+  
+  def best_tipper 
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip #compare 
+    end 
+    best_tipped_meal.customer 
+  end 
 
 end
